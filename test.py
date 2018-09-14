c6e86e6e519e9a1cc6d65c3a2b2a715659146c25
gavinsyw <gavinsyw@163.com>
2018-09-15 07:57:18 +0800

    update for test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import alg1_1
 import world
 
-simulator = world.World(alg1_1.PriorityListAlgorithm().alg, 10000)
+simulator = world.World(alg1_1.PriorityListAlgorithm().alg, 3600*8)
 simulator.simulate()
