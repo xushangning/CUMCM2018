3abb0c866cb05e1e90772a9e618854483e7d6f4b
Shining Xu <xushangning@sjtu.edu.cn>
2018-09-15 14:51:32 +0800

    Handle washing

--- ga.py
+++ ga.py
@@ -14,13 +14,27 @@ def decode(chromosome):
     """
     def alg(entity_dict, clock):
         target_cnc = alg.chromosome[alg.index]
+
+        # if the last instruction is supplying cargo, then do the washing,
+        # except that the cargo is the first one supplied to the CNC
+        if ((entity_dict['RGV'].last_inst
+                == rgv.RGV_modecode_rev['supply cargo 1']
+                or entity_dict['RGV'].last_inst
+                == rgv.RGV_modecode_rev['supply cargo 2'])
+                and alg.should_wash):
+            return rgv.RGV_modecode_rev['wash']
         # if RGV is right in front of the target CNC
-        if entity_dict['RGV'].posi == (target_cnc + 1) // 2:
+        elif entity_dict['RGV'].posi == (target_cnc + 1) // 2:
             if (entity_dict['CNC'][target_cnc].status
                     == cnc.CNC_modecode_rev['processing']):
                 return rgv.RGV_modecode_rev['idle']
             else:
                 alg.index += 1   # cargo supplied
+                # if this is the first cargo supplied to the CNC
+                # DON'T WASH
+                if (entity_dict['CNC'][target_cnc].status
+                        == cnc.CNC_modecode_rev['idle']):
+                    alg.should_wash = False
                 if target_cnc % 2:
                     return rgv.RGV_modecode_rev['supply cargo 1']
                 else:
@@ -31,11 +45,13 @@ def decode(chromosome):
 
     alg.chromosome = chromosome
     alg.index = 0   # subscript the chromosome
+    # whether supplying cargo should be followed by washing
+    alg.should_wash = False
     return alg
 
 
 if __name__ == '__main__':
     test_chromosome = np.random.randint(1, 9, 200, 'int64')
     print(test_chromosome)
-    simulator = world.World(decode(test_chromosome), 3600 * 8)
+    simulator = world.World(decode(test_chromosome), 900)
     simulator.simulate()
