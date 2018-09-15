7a0400e36304c307092b741afd81a2587c998e2c
gavinsyw <gavinsyw@163.com>
2018-09-15 13:30:09 +0800

    A classifier solution for question 1 (tmp save)

---
+++ alg1_2.py
@@ -0,0 +1,9 @@
+# A deep-learning classifier for decision making.
+# All CNCs will be classified into 2 status: be served next or not.
+# The parameters for the classifier can be:
+# 1. The working status of the CNC. (How much time left for processing)
+# 2. The position of the RGV.
+# 3. The load time for this CNC.
+# 4. Will this CNC complete the final product.
+# The return will be a whole sequence of the work line.
+
