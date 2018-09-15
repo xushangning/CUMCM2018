90f54d4dec298e68da8c03c0728fc180d19c6705
Shining Xu <xushangning@sjtu.edu.cn>
2018-09-15 12:58:18 +0800

    Add a function to decode chromosome to generate a schedule
    The schedule generated do not include the step of washing.

---
+++ ga.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+import cnc
+import rgv
+import world
+
+
+def decode(chromosome):
+    """
+    Decode chromosome to generate a schedule.
+
+    :param chromosome:
+    :return:
+    """
+    def alg(entity_dict, clock):
+        target_cnc = alg.chromosome[alg.index]
+        # if RGV is right in front of the target CNC
+        if entity_dict['RGV'].posi == (target_cnc + 1) // 2:
+            if (entity_dict['CNC'][target_cnc].status
+                    == cnc.CNC_modecode_rev['processing']):
+                return rgv.RGV_modecode_rev['idle']
+            else:
+                alg.index += 1   # cargo supplied
+                print(alg.index)
+                if target_cnc % 2:
+                    return rgv.RGV_modecode_rev['supply cargo 1']
+                else:
+                    return rgv.RGV_modecode_rev['supply cargo 2']
+        else:
+            steps = (target_cnc + 1) // 2 - entity_dict['RGV'].posi
+            if steps > 0:
+                return rgv.RGV_modecode_rev['move ' + str(steps) + ' right']
+            else:
+                return rgv.RGV_modecode_rev['move ' + str(abs(steps)) + ' left']
+
+    alg.chromosome = chromosome
+    alg.index = 0   # subscript the chromosome
+    return alg
+
+
+if __name__ == '__main__':
+    test_chromosome = np.random.randint(1, 9, 200, 'int64')
+    print(test_chromosome)
+    simulator = world.World(decode(test_chromosome), 3600 * 8)
+    simulator.simulate()
