6e641c7d7e57ccd25df85fe53a0170fc963cfd9a
gavinsyw <gavinsyw@163.com>
2018-09-16 16:45:17 +0800

    Update ga2

--- ga2.py
+++ ga2.py
@@ -6,7 +6,7 @@ import cnc
 import rgv
 import world
 import cargo
-from ga import select_new_population, crossover
+from ga import select_new_population
 
 
 def decode(chromosome_pair):
@@ -16,7 +16,7 @@ def decode(chromosome_pair):
                     == cargo.Cargo_modecode_rev['ready']):
                 return rgv.RGV_modecode_rev['wash']
             elif (entity_dict['RGV'].carry.status
-                    == cargo.Cargo_modecode_rev['half']):
+                  == cargo.Cargo_modecode_rev['half']):
                 # indicate whether the chosen CNC comes from the first
                 # chromosome or the second chromosome
                 chosen_cnc_index = 1
@@ -28,8 +28,8 @@ def decode(chromosome_pair):
                     == cnc.CNC_modecode_rev['idle']):
                 chosen_cnc_index = 0
             else:
-                waiting_time = [0, 0]   # waiting time for 2 CNCs
-                steps = [               # steps needed to move to a CNC
+                waiting_time = [0, 0]  # waiting time for 2 CNCs
+                steps = [  # steps needed to move to a CNC
                     (cncs[0] + 1) // 2 - entity_dict['RGV'].posi,
                     (cncs[1] + 1) // 2 - entity_dict['RGV'].posi,
                 ]
@@ -113,17 +113,50 @@ def fitness(chromosome_pairs, template):
     probability = np.exp(fitness_values) / np.sum(np.exp(fitness_values))
     # 得到每个染色体被选中的累积概率
     cum_probability = np.cumsum(probability)
+    pool.close()
+    pool.join()
     return fitness_values, cum_probability
 
 
-def mutation(population, DNA_bases, Pm=0.05):
+def crossover(population, pc=0.5):
     """
-    :param population: 经交叉后得到的种群
-    :param Pm: 变异概率默认是0.01
-    :return: 经变异操作后的新种群
+    :param population: 新种群
+    :param pc: 交叉概率默认是0.8
+    :return: 交叉后得到的新种群
     """
-    update_population = np.copy(population)
+    # 根据交叉概率计算需要进行交叉的个体个数
     m, n = population.shape
+    n = n // 4
+    numbers = np.uint8(m * pc)
+    # 确保进行交叉的染色体个数是偶数个
+    if numbers % 2:
+        numbers += 1
+    # 交叉后得到的新种群
+    update_population = np.zeros(population.shape, dtype=np.uint8)
+    # 产生随机索引
+    index = random.sample(range(m), numbers)
+    # 不进行交叉的染色体进行复制
+    for i in range(m):
+        if not index.__contains__(i):
+            update_population[i, :] = population[i, :]
+    # crossover
+    while len(index) > 0:
+        a = index.pop()
+        b = index.pop()
+        # 随机产生一个交叉点
+        crossover_point = random.sample(range(1, n), 1)
+        crossover_point = crossover_point[0]
+        # one-single-point crossover
+        update_population[a, 0:crossover_point * 4] = population[a, 0:crossover_point * 4]
+        update_population[a, crossover_point * 4:] = population[b, crossover_point * 4:]
+        update_population[b, 0:crossover_point * 4] = population[b, 0:crossover_point * 4]
+        update_population[b, crossover_point * 4:] = population[a, crossover_point * 4:]
+    return update_population
+
+
+def mutation(population, DNA_bases, Pm=0.1):
+    m, n = population.shape
+    n = n // 4
     # 计算需要变异的基因个数
     gene_num = np.uint8(m * n * Pm)
     # 将所有的基因按照序号进行10进制编码，则共有m*n个基因
@@ -136,24 +169,40 @@ def mutation(population, DNA_bases, Pm=0.05):
         # 确定变异基因位于当前染色体的第几个基因位
         geneIndex = gene % n
         # mutation
-        update_population[chromosomeIndex, geneIndex] = DNA_bases[
-            np.random.randint(len(DNA_bases), dtype='int')
-        ]
-    return update_population
+        # update_population[chromosomeIndex, geneIndex] = DNA_bases[
+        #     np.random.randint(len(DNA_bases), dtype='int')
+        # ]
+        np.random.shuffle(population[chromosomeIndex, geneIndex:geneIndex + 4])
+
+    return population
+
+
+def expand(ch):
+    p, m, n = ch.shape
+    res = [None] * p
+    for k in range(p):
+        res[k] = np.zeros((m, n * 40))
+        for i in range(m):
+            res[k][i] = list(ch[k][i]) * 40
+    res = np.array(res, 'int8')
+    return res
 
 
 def ga2(DNA_bases_group, template, max_iter):
-    chromosome_length = 280
+    chromosome_length = 8
     chromosome_pairs = [
-         initial_population(chromosome_length, 50, DNA_bases_group[0]),
-         initial_population(chromosome_length, 50, DNA_bases_group[1])
+        initial_population(chromosome_length, 50, DNA_bases_group[0]),
+        initial_population(chromosome_length, 50, DNA_bases_group[1])
     ]
-    chromosome_pairs[0].append([1, 4, 5, 6] * 70)
-    chromosome_pairs[1].append([2, 3, 7, 8] * 70)
+    # chromosome_pairs[0].append([1, 4, 5, 6] * 2)
+    # chromosome_pairs[1].append([2, 3, 7, 8] * 2)
+    # chromosome_pairs[0].append([1, 5, 4, 6, 6, 5, 4, 1])
+    # chromosome_pairs[1].append([8, 7, 3, 2, 2, 3, 7, 8])
     chromosome_pairs = np.array(chromosome_pairs, 'int8')
+    e_chromosome_pairs = expand(chromosome_pairs)
 
     for iteration in range(max_iter):
-        cum_proba = fitness(chromosome_pairs, template)[1]
+        cum_proba = fitness(e_chromosome_pairs, template)[1]
         new_populations = np.array([
             select_new_population(chromosome_pairs[0], cum_proba),
             select_new_population(chromosome_pairs[1], cum_proba),
@@ -162,12 +211,19 @@ def ga2(DNA_bases_group, template, max_iter):
             crossover(new_populations[0]),
             crossover(new_populations[1])
         ], 'int8')
-        mutated_population = np.array([
+        chromosome_pairs = np.array([
             mutation(crossover_population[0], DNA_bases_group[0]),
             mutation(crossover_population[1], DNA_bases_group[1])
         ], 'int8')
-        chromosome_pairs = mutated_population
-        fitness_values = fitness(chromosome_pairs, template)[0]
+        # chromosome_pairs = list(chromosome_pairs)
+        # chromosome_pairs[0] = list(chromosome_pairs[0])
+        # chromosome_pairs[1] = list(chromosome_pairs[1])
+        # chromosome_pairs[0].append([1, 4, 5, 6] * 2)
+        # chromosome_pairs[1].append([2, 3, 7, 8] * 2)
+        # chromosome_pairs = np.array(chromosome_pairs, 'int8')
+        e_chromosome_pairs = expand(chromosome_pairs)
+        # print(e_chromosome_pairs.shape)
+        fitness_values = fitness(e_chromosome_pairs, template)[0]
         fitness_values.sort()
         print(iteration, fitness_values)
 
@@ -175,4 +231,4 @@ def ga2(DNA_bases_group, template, max_iter):
 if __name__ == '__main__':
     template = [1, 2, 2, 1, 1, 1, 2, 2]
     cnc_type = [[1, 4, 5, 6], [2, 3, 7, 8]]
-    ga2(cnc_type, template, 10)
+    ga2(cnc_type, template, 100)
